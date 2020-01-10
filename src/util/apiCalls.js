193aa009b3cc566bc98999a4dded5896a80f3ba5
Allison Wagner <allisonjw01@gmail.com>
2020-01-10 11:59:09 -0700

    Cleans up api file and leaves last api call commented out until ready for images

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -3,11 +3,11 @@ const apiKey = `?key=$2a$10$UN/Up5FVuauhCHKM/olg7efLQBTNWpkOjodpao2ICW0zo5V9zKAr
 export const getPotterApi = async path => {
     const url = `https://www.potterapi.com/v1/${path}${apiKey}`;
     const response = await fetch(url);
-    const data = await response.json();
     if (!response.ok) {
       throw Error('Error fetching Potter Api');
     }
-    return await Promise.all(data);
+    const data = await response.json();
+    return data
 };
 
 // export const getHerokuApi = async (url) => {
