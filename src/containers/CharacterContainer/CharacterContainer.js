b6f43b554363543efdc6ccc65c19c7ccfd3ba9b7
Allison Wagner <allisonjw01@gmail.com>
2020-01-11 12:41:51 -0700

    Passes character information to character card

--- src/containers/CharacterContainer/CharacterContainer.js
+++ src/containers/CharacterContainer/CharacterContainer.js
@@ -2,29 +2,27 @@ import React from 'react';
 import './CharacterContainer.scss';
 import CharacterCard from '../../components/CharacterCard/CharacterCard';
 import { connect } from 'react-redux';
+// import { PropTypes } from 'prop-types';
+
 
 export const CharacterContainer = props => {
   const {  allCharacters } = props
-  console.log(allCharacters)
 
     const displayCharacters = allCharacters.map(character => {
-            return (
+          return (
             <CharacterCard
               {...character}
               key={character.id}
             />
-            )
-          })
-
+          );
+    });
 
       return (
-          <>
           <section className="section_houses-characters">
                     {displayCharacters}
          </section>
-          </>
       )
-  }
+  };
 
 
 export const mapStateToProps = state => ({
