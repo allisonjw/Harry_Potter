38df89a16e0db01f84a275f392acd6338a5c4257
Allison Wagner <allisonjw01@gmail.com>
2020-01-11 17:07:30 -0700

    Adds setup for action test

---
+++ src/actions/actions.test.js
@@ -0,0 +1,32 @@
+import * as actions from '../actions';
+
+describe('actions', () => {
+    it('should have a type of ADD_CHARACTERS', () => {
+
+    });
+
+    it.skip('should have a type of ADD_HOUSES', () => {
+
+    });
+
+    it.skip('should have a type of ADD_SPELLS', () => {
+
+    });
+
+    it.skip('should have a type of SET_LOADING', () => {
+
+    });
+
+    it.skip('should have a type of SET_CHARACTERS', () => {
+
+    });
+
+    it.skip('should have a type of ADD_FAVORITE', () => {
+
+    });
+
+    it.skip('should have a type of DELETE_FAVORITE', () => {
+
+    });
+
+});
