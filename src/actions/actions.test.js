1590104d7ba394b69e1fe0fb3a143b4161831c4e
Allison Wagner <allisonjw01@gmail.com>
2020-01-11 17:46:37 -0700

    Adds test for all actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,31 +2,78 @@ import * as actions from '../actions';
 
 describe('actions', () => {
     it('should have a type of ADD_CHARACTERS', () => {
+        const characters = [{}, {}, {}];
+        const expectedAction = {
+            type: 'ADD_CHARACTERS',
+            characters
+        };
+    const result = actions.addCharacters(characters);
+    expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of ADD_HOUSES', () => {
+        const houses = [{}, {}, {}];
+        const expectedAction = {
+            type: 'ADD_HOUSES',
+            houses
+        };
+    const result = actions.addHouses(houses);
+    expect(result).toEqual(expectedAction);
+    });
+
+    it('should have a type of ADD_SPELLS', () => {
+        const spells = [{}, {}, {}];
+        const expectedAction = {
+            type: 'ADD_SPELLS',
+            spells
+        };
+    const result = actions.addSpells(spells);
+    expect(result).toEqual(expectedAction);
 
     });
 
-    it.skip('should have a type of ADD_HOUSES', () => {
+    it('should have a type of SET_LOADING', () => {
+        const isLoading = true;
+        const expectedAction = {
+            type: 'SET_LOADING',
+            isLoading
+        };
+    const result = actions.isLoading(isLoading);
+    expect(result).toEqual(expectedAction);
 
     });
 
-    it.skip('should have a type of ADD_SPELLS', () => {
+    it('should have a type of SET_CHARACTERS', () => {
+        const filteredStudents = [{}, {}, {}];
+        const expectedAction = {
+            type: 'SET_CHARACTERS',
+            filteredStudents
+        };
+    const result = actions.addHouseCharacters(filteredStudents);
+    expect(result).toEqual(expectedAction);
 
     });
 
-    it.skip('should have a type of SET_LOADING', () => {
+    it('should have a type of ADD_FAVORITE', () => {
+        const id = '5a0fa4daae5bc100213c232e';
+        const expectedAction = {
+            type: 'ADD_FAVORITE',
+            id
+        };
 
+    const result = actions.addFavorite(id);
+    expect(result).toEqual(expectedAction);
     });
 
-    it.skip('should have a type of SET_CHARACTERS', () => {
+    it('should have a type of DELETE_FAVORITE', () => {
+        const id = '5a0fa4daae5bc100213c232e';
+        const expectedAction = {
+            type: 'DELETE_FAVORITE',
+            id
+        };
 
-    });
-
-    it.skip('should have a type of ADD_FAVORITE', () => {
-
-    });
-
-    it.skip('should have a type of DELETE_FAVORITE', () => {
-
+    const result = actions.deleteFavorite(id);
+    expect(result).toEqual(expectedAction);
     });
 
 });
