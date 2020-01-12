630c1c4dfa717db2565d401b79428c88d764d0ae
Allison Wagner <allisonjw01@gmail.com>
2020-01-11 18:07:30 -0700

    Removes extra lines

--- src/reducers/housesReducer.test.js
+++ src/reducers/housesReducer.test.js
@@ -12,20 +12,21 @@ describe('housesReducer', () => {
     const action = {
         type: 'ADD_HOUSES',
         houses: [{
-            _id: "5a05e2b252f721a3cf2ea33f",
-            name: "Gryffindor",
-            mascot: "lion",
-            headOfHouse: "Minerva McGonagall",
-            houseGhost: "Nearly Headless Nick",
-            founder: "Goderic Gryffindor",
-            __v: 0,
-            school: "Hogwarts School of Witchcraft and Wizardry",
-            members: ["5a0fa648ae5bc100213c2332",
-            "5a0fa67dae5bc100213c2333",
-            "5a0fa7dcae5bc100213c2338",
-            "5a107e1ae0686c0021283b19"]
+        _id: "5a05e2b252f721a3cf2ea33f",
+        name: "Gryffindor",
+        mascot: "lion",
+        headOfHouse: "Minerva McGonagall",
+        houseGhost: "Nearly Headless Nick",
+        founder: "Goderic Gryffindor",
+        __v: 0,
+        school: "Hogwarts School of Witchcraft and Wizardry",
+        members: ["5a0fa648ae5bc100213c2332",
+        "5a0fa67dae5bc100213c2333",
+        "5a0fa7dcae5bc100213c2338",
+        "5a107e1ae0686c0021283b19"]
         }]
-      }
+      };
+
       const expected = [{
         _id: "5a05e2b252f721a3cf2ea33f",
         name: "Gryffindor",
@@ -42,8 +43,6 @@ describe('housesReducer', () => {
       }]
       const result = housesReducer([], action);
       expect(result).toEqual(expected);
-
   });
-  
 
 });  
