8858ebb3c890fc320962a43937e77f0aaa6333e0
Allison Wagner <allisonjw01@gmail.com>
2020-01-18 13:49:52 -0700

    Adds house crest to favorites

--- src/containers/Favorites/Favorite.js
+++ src/containers/Favorites/Favorite.js
@@ -5,11 +5,13 @@ import { PropTypes } from 'prop-types';
 
 export const Favorite = (props) => {
     const { name, house, dumbledoresArmy, bloodStatus, isLoading } = props
+    const houseCrest = house || 'hogwarts';
     return (
         <>
             {isLoading && <img className='loading-img' src='https://media.giphy.com/media/JonSfMu0Kzs5K9Wu7I/giphy.gif' alt='loading' />}
             {!isLoading && <article className="article_favorite-card">
                 <h3>NAME:  {name}</h3>
+                <img alt="house crest" className="favorite_houseCrest" src={require(`../../images/${houseCrest}.png`)}/>
                 <h3>HOUSE: {house}</h3>
                 <h3>DUMBLEDORE's ARMY: {dumbledoresArmy}</h3>
                 <h3>BLOOD STATUS: {bloodStatus}"</h3>
